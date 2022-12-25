470ab533ace14e20af2eb9fa2e70ca6ca23a986c
Farshid-997 <arnob.farshid@gmail.com>
2022-12-26 02:17:03 +0600

    change logo and favicon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import './Header.css';
-import logo from '../../images/14-08.png';
-import SearchBar from '../SearchBar/SearchBar';
-import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
-import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+import logo from "../../images/MainLogo.png";
+import SearchBar from "../SearchBar/SearchBar";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 
 const Header = () => {
-    return (
-        <div className="container header d-flex justify-content-between">
-            <nav>
-                <Link to="/"><img src= {logo} alt="logo"></img></Link>
-            </nav>
-            <nav>
-                <Link to="/home"><ShoppingCartOutlinedIcon/><span className=""> CART (0)</span></Link>
-                <Link to="/orders"><FavoriteBorderOutlinedIcon/><span className=""> WISHLIST (0)</span></Link>
-                <Link to="/inventory/addProduct"><span className="">LOGIN</span></Link>
-            </nav>
-        </div>
-    );
+  return (
+    <div className="container header d-flex justify-content-between">
+      <nav>
+        <Link to="/">
+          <img
+            src={logo}
+            alt="logo"
+            style={{ width: "70px", height: "70px" }}
+          ></img>
+        </Link>
+      </nav>
+      <nav>
+        <Link to="/home">
+          <ShoppingCartOutlinedIcon />
+          <span className=""> CART (0)</span>
+        </Link>
+        <Link to="/orders">
+          <FavoriteBorderOutlinedIcon />
+          <span className=""> WISHLIST (0)</span>
+        </Link>
+        <Link to="/inventory/addProduct">
+          <span className="">LOGIN</span>
+        </Link>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;

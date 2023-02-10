721bd285b4928710480b0c61b927e959b5c0d190
Farshid-997 <arnob.farshid@gmail.com>
2023-02-10 15:48:10 +0600

    added

--- src/components/FooterCol/FooterCol.js
+++ src/components/FooterCol/FooterCol.js
@@ -4,7 +4,16 @@ import { Link } from "react-router-dom";
 const FooterCol = (props) => {
   return (
     <div className="col-md-3 ">
-      <h6>{props.title ? props.title : " "}</h6>
+      <h6
+        style={{
+          color: "#e8b2a3",
+          fontFamily: `"Hind Siliguri", sans-serif`,
+          fontWeight: "800",
+          fontSize: "22px",
+        }}
+      >
+        {props.title ? props.title : " "}
+      </h6>
       <ul className="list-unstyled">
         {props.items.map((item) => (
           <li>
@@ -12,7 +21,8 @@ const FooterCol = (props) => {
               to={item.link}
               style={{
                 textDecoration: "none",
-                color: "#26235d;",
+                color: "#26235d",
+                fontWeight: "600",
                 fontFamily: `"Hind Siliguri", sans-serif`,
               }}
             >

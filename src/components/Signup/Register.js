a0e71f6d3a6cf157a66a5462759463c672b6a7c6
Farshid-997 <arnob.farshid@gmail.com>
2023-02-25 06:14:14 +0600

    added

--- src/components/Signup/Register.js
+++ src/components/Signup/Register.js
@@ -1,149 +1,101 @@
 import * as React from "react";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import { useForm } from "react-hook-form";
+import regimg from "./134945-zpunet-icon.mp4"
+import "./Register.css"
+import { Container, Row,Col} from "react-bootstrap";
 
-// function Copyright(props) {
-//   return (
-//     <Typography
-//       variant="body2"
-//       color="text.secondary"
-//       align="center"
-//       {...props}
-//     >
-//       {"Copyright © "}
-//       <Link color="inherit" href="https://mui.com/">
-//         Your Website
-//       </Link>{" "}
-//       {new Date().getFullYear()}
-//       {"."}
-//     </Typography>
-//   );
-// }
-
-const theme = createTheme();
 
 export default function Register() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
+  const { register, handleSubmit, errors, watch } = useForm();
+  const validatePhoneNumber = (value) => {
+    const regex = /^[0-9\b]+$/; // Regex to allow only numbers
+    if (!value) {
+      return "Mobile number is required";
+    } else if (value.length < 10) {
+      return "Mobile number must be at least 10 digits";
+    } else if (!regex.test(value)) {
+      return "Invalid mobile number";
+    }
   };
 
+  const onSubmit = (data) => console.log(data);
   return (
     <>
+    
       <Header></Header>
+<Container>
+<h1 className="signup-header">Please Register Here</h1>
+<Row>
+<Col xs={12} md={6}>
+<form>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            placeholder="Enter your firstName"
+            {...register("firstName", { required: true, maxLength: 20 })}
+          />
 
-      <ThemeProvider theme={theme}>
-        <Container component="main" maxWidth="xs">
-          <CssBaseline />
-          <Box
-            sx={{
-              marginTop: 8,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign up
-            </Typography>
-            <Box
-              component="form"
-              noValidate
-              onSubmit={handleSubmit}
-              sx={{ mt: 3 }}
-            >
-              <Grid container spacing={2}>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    autoComplete="given-name"
-                    name="firstName"
-                    required
-                    fullWidth
-                    id="firstName"
-                    label="First Name"
-                    autoFocus
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="lastName"
-                    label="Last Name"
-                    name="lastName"
-                    autoComplete="family-name"
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    autoComplete="email"
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="new-password"
-                  />
-                </Grid>
-                {/* <Grid item xs={12}>
-                <FormControlLabel
-                  control={
-                    <Checkbox value="allowExtraEmails" color="primary" />
-                  }
-                  label="I want to receive inspiration, marketing promotions and updates via email."
-                />
-              </Grid> */}
-              </Grid>
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Sign Up
-              </Button>
-              <Grid container justifyContent="flex-end">
-                <Grid item>
-                  <Link href="#" variant="body2">
-                    Already have an account? Sign in
-                  </Link>
-                </Grid>
-              </Grid>
-            </Box>
-          </Box>
-          {/* <Copyright sx={{ mt: 5 }} /> */}
-        </Container>
-      </ThemeProvider>
+          <input placeholder="Enter your lastName" {...register("lastName", { required: true, maxLength: 20 })} />
+
+
+          <input
+            placeholder="Enter your password"
+            {...register("password", {
+              required: true,
+              minLength: 8,
+              pattern: {
+                value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+                message:
+                  "Password must contain at least one letter and one number",
+              },
+            })}
+          />
+
+          <input
+            placeholder="confirm password"
+            {...register("confirmpassword", {
+              required: true,
+              minLength: 8,
+              validate: (value) =>
+                value === watch("password") || "The passwords do not match",
+            })}
+          />
+
+          <input
+            placeholder="Enter your address"
+            {...register("address", { required: true, maxLength: 20 })}
+          />
+
+          <input
+            placeholder="Enter your phone number"
+            {...register("phone", { validate: validatePhoneNumber })}
+          />
+
+          <input
+           placeholder="Enter your City"
+           {...register("city", { required: true })} />
+
+         
+          <input type="submit"  className="bg-success border-0"/>
+        </form>
+      </form>
+           
+          </Col>
+
+
+          <Col xs={12} md={6}>
+
+            <video src={regimg} width={400} height={400} loop style={{marginTop:'4rem'}}></video>
+          </Col>
+</Row>
+
+
+
+
+</Container>
+
+
 
       <Footer></Footer>
     </>
